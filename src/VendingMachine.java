6e00a0c3b40f869df7092b55233794c5d66c567b
Dominick Bruno <dbruno@unseen.is>
2020-06-23 01:14:13 -0400

    Complete VendingMachine class.

---
+++ src/VendingMachine.java
@@ -0,0 +1,29 @@
+package snackBar;
+
+public class VendingMachine
+{
+	private int maxId = 0;
+	private String name;
+
+	public VendngMachine(String name)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+	}
+
+	public int getId()
+	{
+		return Id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void etName(String name)
+	{
+		this.name = name;
+	}
+}
