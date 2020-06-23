8f7527394a0633c9de50c639d83f85d7f8246a6b
Dominick Bruno <dbruno@unseen.is>
2020-06-23 01:04:45 -0400

    Complete Snack class.

---
+++ src/Snack.java
@@ -0,0 +1,80 @@
+package snackBar;
+
+public class Snack 
+{
+
+	private static int maxId = 0;
+	private int id;
+	private String name;
+	private int quantity;
+	private double cost;
+	private int vmId;
+
+	public Snack(String name, int quantity, double cost, int vmId)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+		this.quantity = quantity;
+		this.cost = cost;
+		this vmId = vmId;
+	}
+
+	public int getId()
+	{
+		return Id;
+	}
+
+	public String getName()
+	{
+		return name
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public double getCost()
+	{
+		return cost;
+	}
+
+	public void setCost(double cost)
+	{
+		this.cost = cost;
+	}
+
+	public int getvmId()
+	{
+		return vmId;
+	}
+
+	public void setvmId(int vmId)
+	{
+		this.vmId = vmId;
+	}
+
+	public int getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(int quantity)
+	{
+		this.quantity += quantity;
+	}
+
+	public void buySnack(int numToBuy)
+	{
+		this.quantity -= numToBuy);
+	}
+
+	public double getTotalCost(int quantity)
+	{
+		return (this.cost) * quantity;
+	}
+}
+
+	
+	
